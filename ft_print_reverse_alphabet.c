2f6bd4f020e389357bae8ae733cbb66683eeaf9f
Adryene Marques <91609892+adryenemarques@users.noreply.github.com>
2022-04-23 16:00:59 -0300

    ex02

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: admarque <admarque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 19:37:15 by admarque          #+#    #+#             */
+/*   Updated: 2022/04/18 23:19:37 by admarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	int	i;
+
+	i = 'z';
+	while (i >= 'a')
+	{
+		write(1, &i, 1);
+		i --;
+	}
+}
+ int main(){
+	 ft_print_reverse_alphabet();
+ }
