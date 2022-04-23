29f8616389213a06c228b6036e3149e528ff9643
Adryene Marques <91609892+adryenemarques@users.noreply.github.com>
2022-04-23 16:02:08 -0300

    ex03
    lista c00

---
+++ ft_print_numbers.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: admarque <admarque@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/14 21:48:38 by admarque          #+#    #+#             */
+/*   Updated: 2022/04/18 23:25:25 by admarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_numbers(void)
+{
+	int	number;
+
+	number = '0';
+	while (number <= '9')
+	{
+		write(1, &number, 1);
+		number ++;
+	}
+}
+
+int main(){
+	ft_print_numbers();
+}
